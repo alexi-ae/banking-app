373e468921c7c3adc7c930b0ed3fbc5525065d74
alexi-ae <alexi.acunaespino@gmail.com>
2024-10-19 11:47:58 -0500

    feat add endpoint to view movements

--- src/main/java/com/alexiae/banking/controller/TransactionController.java
+++ src/main/java/com/alexiae/banking/controller/TransactionController.java
@@ -2,10 +2,14 @@ package com.alexiae.banking.controller;
 
 
 import com.alexiae.banking.facede.TransactionFacade;
+import com.alexiae.banking.model.api.TransactionMovementInfoResponse;
 import com.alexiae.banking.model.api.TransactionRequest;
 import com.alexiae.banking.model.api.TransactionResponse;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -24,6 +28,13 @@ public class TransactionController {
   public TransactionResponse send(@RequestBody TransactionRequest request,
       @RequestAttribute("customerId") String customerId) {
     return transactionFacade.send(Long.parseLong(customerId), request);
+  }
+
+  @GetMapping("/movements/account/{accountId}")
+  public List<TransactionMovementInfoResponse> getMovementsByAccountId(
+      @PathVariable(required = true) Long accountId,
+      @RequestAttribute("customerId") String customerId) {
+    return transactionFacade.getMovementsByAccountId(accountId, Long.parseLong(customerId));
   }
 
 }
--- src/main/java/com/alexiae/banking/facede/TransactionFacade.java
+++ src/main/java/com/alexiae/banking/facede/TransactionFacade.java
@@ -1,15 +1,24 @@
 package com.alexiae.banking.facede;
 
+import com.alexiae.banking.exception.ApiRestException;
+import com.alexiae.banking.exception.ErrorReason;
+import com.alexiae.banking.exception.ErrorSource;
+import com.alexiae.banking.model.api.TransactionMovementInfoResponse;
 import com.alexiae.banking.model.api.TransactionRequest;
 import com.alexiae.banking.model.api.TransactionResponse;
 import com.alexiae.banking.model.entity.Account;
 import com.alexiae.banking.model.entity.Customer;
 import com.alexiae.banking.model.entity.Transaction;
+import com.alexiae.banking.model.entity.TransactionMovement;
 import com.alexiae.banking.model.enums.AccountStatus;
+import com.alexiae.banking.model.enums.MovementType;
 import com.alexiae.banking.model.enums.TransactionStatus;
 import com.alexiae.banking.service.AccountService;
 import com.alexiae.banking.service.CustomerService;
+import com.alexiae.banking.service.TransactionMovementService;
 import com.alexiae.banking.service.TransactionService;
+import com.alexiae.banking.utils.FormatUtils;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -26,6 +35,9 @@ public class TransactionFacade {
   @Autowired
   private CustomerService customerService;
 
+  @Autowired
+  private TransactionMovementService transactionMovementService;
+
   public TransactionResponse send(Long customerId, TransactionRequest request) {
     Customer customer = customerService.listById(customerId);
     Optional<Account> accountOptional = customer.getAccounts().stream()
@@ -33,6 +45,7 @@ public class TransactionFacade {
     if (accountOptional.isEmpty()) {
       throw new RuntimeException("Fraud by transaction origin account");
     }
+
     Account originAccount = accountService.getByAccountNumber(request.getOriginAccount());
     Account destinationAccount = accountService.getByAccountNumber(request.getDestinationAccount());
     validateProcess(request, originAccount, destinationAccount);
@@ -40,8 +53,6 @@ public class TransactionFacade {
     Transaction transaction = new Transaction();
     transaction.setAmount(request.getAmount());
     transaction.setDescription(request.getDescription());
-    transaction.setOriginAccount(originAccount);
-    transaction.setDestinationAccount(destinationAccount);
     transaction.setStatus(TransactionStatus.PENDING);
     transaction = transactionService.create(transaction);
 
@@ -51,9 +62,25 @@ public class TransactionFacade {
         destinationAccount.getCurrentBalance().add(request.getAmount()));
     accountService.update(originAccount);
     accountService.update(destinationAccount);
+
+    TransactionMovement trxMovementOrigin = new TransactionMovement();
+    trxMovementOrigin.setTransaction(transaction);
+    trxMovementOrigin.setAccount(originAccount);
+    trxMovementOrigin.setAmount(transaction.getAmount().negate());
+    trxMovementOrigin.setMovementType(MovementType.SENT);
+    transactionMovementService.create(trxMovementOrigin);
+
+    TransactionMovement trxMovementDestiny = new TransactionMovement();
+    trxMovementDestiny.setTransaction(transaction);
+    trxMovementDestiny.setAccount(destinationAccount);
+    trxMovementDestiny.setAmount(transaction.getAmount());
+    trxMovementDestiny.setMovementType(MovementType.RECEIVED);
+    transactionMovementService.create(trxMovementDestiny);
     transaction.setStatus(TransactionStatus.SUCCESS);
     transactionService.create(transaction);
-    String operationCode = String.format("%-15d", transaction.getId()).replace(' ', '0');
+
+    String operationCode = FormatUtils.generateOperationCode(10, transaction.getId());
+    //String operationCode = String.format("%-15d", transaction.getId()).replace(' ', '0');
     return TransactionResponse.builder().originAccount(request.getOriginAccount())
         .destinationAccount(request.getDestinationAccount()).description(request.getDescription())
         .amount(request.getAmount()).operationCode(operationCode).build();
@@ -77,5 +104,26 @@ public class TransactionFacade {
       throw new RuntimeException("todavia no se aplica tipo de cambio");
     }
 
+  }
+
+  public List<TransactionMovementInfoResponse> getMovementsByAccountId(Long accountId,
+      long customerId) {
+    boolean customerHasAccount = accountService.existsByIdAndCustomerId(accountId, customerId);
+    if (Boolean.FALSE.equals(customerHasAccount)) {
+      throw ApiRestException.builder().source(ErrorSource.BUSINESS_SERVICE)
+          .reason(ErrorReason.BAD_REQUEST).build();
+    }
+
+    return transactionMovementService.getByAccountId(accountId).stream().map(
+        transactionMovement -> TransactionMovementInfoResponse.builder()
+            .operationCode(
+                FormatUtils.generateOperationCode(10, transactionMovement.getTransaction().getId())
+                    + "-" + FormatUtils.generateOperationCode(10,
+                    transactionMovement.getId()))
+            .amount(transactionMovement.getAmount())
+            .description(transactionMovement.getTransaction().getDescription())
+            .status(transactionMovement.getTransaction().getStatus())
+            .type(transactionMovement.getMovementType())
+            .creationDate(transactionMovement.getCreationDate()).build()).toList();
   }
 }
